a84a3716ebf1c856b88b4511d3298c431f56a34d
ghaleon7 <76746117+ghaleon7@users.noreply.github.com>
2022-02-03 13:21:31 +0000

    Update Class_Zn.py
    Definidos os inteiros módulo N e suas principais operações.

--- Class_Zn.py
+++ Class_Zn.py
@@ -1,93 +1,99 @@
 # -*- coding: utf-8 -*-
 """
-Este ficheiro Python serve para construir uma classe chamada Zn para me 
-familiarizar com a sintaxe Python e com a construção de classes nela.
-A ideia é construir uma classe correspondente às classes de congruência
-módulo n. 
+Com este ficheiro Python, a ideia é construir os inteiros módulo n.
 """
-#class MyClass:
-#    # construtor
-#    def __init__(self, x):
-#        """
-#        """
-#        if not isinstance(x, str):
-#            raise TypeError()
-#        self.a = x + '_attr'
-#     
-#    # podemos entender o "self" como apontando para a instância em causa quando 
-#    # o método é chamado
-#    def has_attr(self, y):
-#        """
-#        """
-#        if not y.endswith('_attr'):
-#            raise ValueError()
-#        return self.a == y
-#
-## cria uma instância da classe MyClass, por trás é chamado o método __init__   
-#inst = MyClass('xpto')
-## podemos aceder ao atributo "a"
-#print(inst.a)
-#
-## os seguintes são equivalentes, normalmente usamos o primeiro
-#result = inst.has_attr('aaa_attr')
-#result1 = MyClass.has_attr(inst, 'aaa_attr')
 
 # =============================================================================
-# 
+# FUNÇÕES AUXILIARES
 # =============================================================================
-from math import gcd
+
+def gcdxy(a, b):
+    """
+    Esta função calcula o máximo divisor comum entre a e b 
+    de forma a resolver a equação ax + by = mdc(a,b).
+    Visto em: https://www.geeksforgeeks.org/python-program-
+    for-basic-and-extended-euclidean-algorithms-2/
+    Argumentos:
+        - a,b - int
+    Devolve:
+        - gcd, x, y - int
+    Exemplo:
+        gcdxy(15,8)
+            >> (1, -1, 2)
+    """
+    if a == 0 :   
+        return b,0,1         
+    gcd,x_i,y_i = gcdxy(b%a, a)  
+    x,y = y_i - (b//a) * x_i, x_i
+    return gcd,x,y 
+
+# =============================================================================
+# CONSTRUÇÃO DA CLASSE
+# =============================================================================
 
 class Zn:
-    N=19
-    def __init__(self, m):
+    def __init__(self, m, N):
         """
+        Inicializa a instância de Z/nZ. Toma dois argumentos m e N. N é o 
+        módulo e "m" é o valor de Z_N que procuramos. 
+        Uma instância de Z_N é um tuplo com o menor representante positivo
+        na primeira coordenada e o módulo na segunda coordenada.
         """
-        if not isinstance(m, int):
-            raise TypeError("Precisamos de inteiros")
-        self.k = m % self.N
+        if not all([isinstance(x, int) for x in (m,N)]):
+            raise TypeError(
+                    """Foram introduzidos os números {} e {}, necessitamos 
+                    que ambos os números sejam do tipo «int»""".format(m,N)
+                    )
+        self.mod = N    
+        self.rep = m % self.mod
+        
+    def show(self):
+        """
+        Mostra a representação da instância enquanto elemento de Z/nZ.
+        """
+        return self.rep, self.mod
         
     def is_invertible(self):
         """
-        Verifica se a instância é um elemento invertível de Zn.
+        Verifica se a instância é um elemento invertível de Z/nZ.
         """
-        return gcd(self.k, self.N) == 1
+        return gcdxy(self.rep, self.mod)[0] == 1
     
     def inverse(self):
         """
-        
+        Devolve o inverso da instância módulo N.
         """
         if not self.is_invertible():
-            raise ValueError("Precisamos de invertíveis")
-        else:
-            for x in range(1, self.N) : 
-                if ((self.k * x) % self.N == 1) : 
-                    return Zn(x)
+            raise ValueError(
+                    """Para se ter um inverso, é necessário que a instância
+                    seja invertível.""")
+        gcd,x,y = gcdxy(self.rep, self.mod)
+        return self.__class__(x, self.mod)
         
     def soma(self, other):
         """
-        
+        Permite somar duas instâncias módulo N.
         """
-        if not isinstance(other, Zn):
-            raise TypeError()
-        result = Zn(self.k + other.k)
-        return result
+        if not isinstance(other, self.__class__):
+            raise TypeError("Só podemos somar duas instâncias da mesma classe.")
+        return self.__class__(self.rep + other.rep, self.mod)
+    
+    def __add__(self,other):
+        """
+        """
+        return self.soma(other)
     
     def produto(self, other):
         """
-        
+        Permite somar duas instâncias módulo N.
         """
-        if not isinstance(other, Zn):
-            raise TypeError()
-        result = Zn(self.k * other.k)
-        return result
-
-
-inst = Zn(19)
-inst2 = Zn(39)
-print(inst.k)
-inst.is_invertible()
-lista_inversos = [Zn(i).inverse().k for i in range(1,Zn.N)]
-print(lista_inversos)
-print(inst.soma(inst2).k)
-print(inst.produto(inst2).k)
-
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                    """Só podemos multiplicar duas instâncias da mesma classe."""
+                    )
+        return self.__class__(self.rep * other.rep, self.mod)
+    
+    def __mul__(self,other):
+        """
+        """
+        return self.produto(other)
